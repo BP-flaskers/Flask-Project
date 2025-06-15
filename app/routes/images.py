b76a8b6aaeeaf0f4e040b882512abd7871f20428
zanwoll <skd03541@gmail.com>
2025-06-15 15:49:13 +0900

    images/rollback

--- app/routes/images.py
+++ app/routes/images.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request, Blueprint
-from app.models import Image, db, ImageType
+from app.models import Image, db
 
 # 이미지 관련 API를 관리하는 Blueprint 생성
 images_bp = Blueprint("images", __name__)
@@ -15,7 +15,7 @@ def create_image():
         # Image 객체 생성 후 DB에 저장
         image = Image(
             url=data["url"],  # 이미지 URL
-            type=ImageType(data["type"]),  # 이미지 타입(예: "main", "sub" 등)
+            type=data["type"],  # 이미지 타입(예: "main", "sub" 등)
         )
         db.session.add(image)
         db.session.commit()
@@ -25,15 +25,12 @@ def create_image():
     except KeyError as e:
         # 필수 필드가 누락된 경우 예외 처리
         return jsonify({"message": f"Missing required field: {str(e)}"}), 400
-    except ValueError as e:
-        # Enum 변환 실패시 예외 처리
-        return jsonify({"message": f"Invalid value for type: {str(e)}"}), 400
 
 
 # [R] 'main' 타입의 첫 번째 이미지를 반환
 @images_bp.route("/image/main", methods=["GET"])
 def get_main_image_route():
     # "main" 타입의 이미지 중 첫 번째 데이터를 가져오기
-    image = Image.query.filter_by(type=ImageType.main).first()
+    image = Image.query.filter_by(type="main").first()
 
     return jsonify({"image": image.url if image else None}), 200
